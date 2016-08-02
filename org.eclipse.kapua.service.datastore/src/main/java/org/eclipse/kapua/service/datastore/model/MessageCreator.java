2b8396fc7b01bb178537a84ccfd1fdfb74d60045
Henryk Konsek <hekonsek@gmail.com>
2016-08-02 14:26:27 +0200

    [#23] Improved message creator JavaDoc.

--- org.eclipse.kapua.service.datastore/src/main/java/org/eclipse/kapua/service/datastore/model/MessageCreator.java
+++ org.eclipse.kapua.service.datastore/src/main/java/org/eclipse/kapua/service/datastore/model/MessageCreator.java
@@ -8,16 +8,31 @@
  *
  * Contributors:
  *     Eurotech - initial API and implementation
+ *     Red Hat - API documentation
  *
  *******************************************************************************/
 package org.eclipse.kapua.service.datastore.model;
 
 import java.util.Date;
 
+/**
+ * Builder object for telemetry message that is supposed to be stored by Kapua.
+ */
 public interface MessageCreator extends StorableCreator<Message>
 {
+
+    /**
+     * Timestamp indicating when telemetry value has been read on the device. The timestamp is expressed using server
+     * clock time - if time on the device is different than on the server, then the timestamp should be converted before
+     * to server time before it is passed to the creator.
+     *
+     * Cannot be null.
+     */
     public Date getTimestamp();
 
+    /**
+     * Setter for timestamp. Cannot be null.
+     */
     public void setTimestamp(Date timestamp);
 
     public Date getReceivedOn();
