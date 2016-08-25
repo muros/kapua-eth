dcce3514f1443de2414a53a35715f07f4f4b25f3
Henryk Konsek <hekonsek@gmail.com>
2016-08-25 13:16:53 +0200

    [#89] Extracted JDBC URL generation logic in a pluggable abstraction.

--- org.eclipse.kapua.commons/src/main/java/org/eclipse/kapua/commons/util/AbstractEntityManagerFactory.java
+++ org.eclipse.kapua.commons/src/main/java/org/eclipse/kapua/commons/util/AbstractEntityManagerFactory.java
@@ -21,6 +21,8 @@ import javax.persistence.Persistence;
 import javax.persistence.spi.PersistenceProvider;
 
 import org.eclipse.kapua.KapuaException;
+import org.eclipse.kapua.commons.jpa.DefaultConfigurableJdbcConnectionUrlResolver;
+import org.eclipse.kapua.commons.jpa.JdbcConnectionUrlResolver;
 import org.eclipse.kapua.commons.setting.system.SystemSetting;
 import org.eclipse.kapua.commons.setting.system.SystemSettingKey;
 import org.slf4j.Logger;
@@ -36,6 +38,8 @@ public abstract class AbstractEntityManagerFactory
     private static final Map<String, String> s_uniqueConstraints = new HashMap<>();
     private EntityManagerFactory             entityManagerFactory;
 
+    private final JdbcConnectionUrlResolver jdbcConnectionUrlResolver = new DefaultConfigurableJdbcConnectionUrlResolver();
+
     protected AbstractEntityManagerFactory(String persistenceUnitName,
                                            String datasourceName,
                                            Map<String, String> uniqueConstraints)
@@ -43,60 +47,12 @@ public abstract class AbstractEntityManagerFactory
         //
         // Initialize the EntityManagerFactory
         try {
-
             SystemSetting config = SystemSetting.getInstance();
-
-            // Mandatory connection parameters
-            String dbName = config.getString(SystemSettingKey.DB_NAME);
-            String dbConnectionScheme = config.getString(SystemSettingKey.DB_CONNECTION_SCHEME);
-            String dbConnectionHost = config.getString(SystemSettingKey.DB_CONNECTION_HOST);
-            String dbConnectionPort = config.getString(SystemSettingKey.DB_CONNECTION_PORT);
-
-            StringBuilder dbConnectionString = new StringBuilder().append(dbConnectionScheme)
-                                                                  .append("://")
-                                                                  .append(dbConnectionHost)
-                                                                  .append(":")
-                                                                  .append(dbConnectionPort)
-                                                                  .append("/")
-                                                                  .append(dbName)
-                                                                  .append("?");
-
-            // Optional connection parameters
-            String useTimezone = config.getString(SystemSettingKey.DB_USE_TIMEZIONE);
-            if (useTimezone != null) {
-                dbConnectionString.append("useTimezone=")
-                                  .append(useTimezone)
-                                  .append("&");
-            }
-
-            String useLegacyDatetimeCode = config.getString(SystemSettingKey.DB_USE_LEGACY_DATETIME_CODE);
-            if (useLegacyDatetimeCode != null) {
-                dbConnectionString.append("useLegacyDatetimeCode=")
-                                  .append(useLegacyDatetimeCode)
-                                  .append("&");
-            }
-
-            String serverTimezone = config.getString(SystemSettingKey.DB_SERVER_TIMEZONE);
-            if (serverTimezone != null) {
-                dbConnectionString.append("serverTimezone=")
-                                  .append(serverTimezone)
-                                  .append("&");
-            }
-
-            String characterEncoding = config.getString(SystemSettingKey.DB_CHAR_ENCODING);
-            if (characterEncoding != null) {
-                dbConnectionString.append("characterEncoding=")
-                                  .append(characterEncoding)
-                                  .append("&");
-            }
-
-            // This deletes the trailing '?' or '&'
-            dbConnectionString.deleteCharAt(dbConnectionString.length() - 1);
 
             //
             // JPA configuration overrides
             Map<String, Object> configOverrides = new HashMap<String, Object>();
-            configOverrides.put("eclipselink.connection-pool.default.url", dbConnectionString.toString());
+            configOverrides.put("eclipselink.connection-pool.default.url", jdbcConnectionUrlResolver.connectionUrl());
             configOverrides.put("eclipselink.connection-pool.default.user", config.getString(SystemSettingKey.DB_USERNAME));
             configOverrides.put("eclipselink.connection-pool.default.password", config.getString(SystemSettingKey.DB_PASSWORD));
 
