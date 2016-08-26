707b273a6b3bd5d558802849977973922f79ee7e
stefanomorson <stefano.morson@eurotech.com>
2016-08-26 23:30:29 +0200

    Enabled unit tests on authentication.shiro project

--- org.eclipse.kapua.service.auth.shiro/src/test/java/org/eclipse/kapua/service/authentication/shiro/AuthenticationServiceTest.java
+++ org.eclipse.kapua.service.auth.shiro/src/test/java/org/eclipse/kapua/service/authentication/shiro/AuthenticationServiceTest.java
@@ -23,7 +23,6 @@ import org.eclipse.kapua.locator.KapuaLocator;
 import org.eclipse.kapua.service.authentication.AuthenticationService;
 import org.eclipse.kapua.service.authentication.UsernamePasswordToken;
 import org.eclipse.kapua.service.authentication.UsernamePasswordTokenFactory;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
