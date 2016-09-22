b6513080e4e14b5c012d51a045a6c83f497cb7d9
stefanomorson <stefano.morson@eurotech.com>
2016-09-22 09:48:36 +0200

    Fixed some file headers

--- org.eclipse.kapua.commons/src/main/java/org/eclipse/kapua/commons/model/id/KapuaIdFactoryImpl.java
+++ org.eclipse.kapua.commons/src/main/java/org/eclipse/kapua/commons/model/id/KapuaIdFactoryImpl.java
@@ -1,12 +1,13 @@
 /*******************************************************************************
- * Copyright (c) 2016 IBM Corporation and others.
+ * Copyright (c) 2011, 2016 Eurotech and/or its affiliates
+ *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  *
  * Contributors:
- *     IBM Corporation - initial API and implementation
+ *     Eurotech - initial API and implementation
  *******************************************************************************/
 package org.eclipse.kapua.commons.model.id;
 
