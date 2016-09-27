689a3c732ae7cb1d4bca53273b6abffce2f58d1f
Claudio Mezzasalma <claudio.mezzasalma@eurotech.com>
2016-09-27 14:20:53 +0200

    Update

--- org.eclipse.kapua.commons/src/main/java/org/eclipse/kapua/commons/model/AbstractKapuaEntity.java
+++ org.eclipse.kapua.commons/src/main/java/org/eclipse/kapua/commons/model/AbstractKapuaEntity.java
@@ -93,7 +93,7 @@ public abstract class AbstractKapuaEntity implements KapuaEntity, Serializable
     {
         this.scopeId = (KapuaEid)scopeId;
     }
-    
+
     public Date getCreatedOn()
     {
         return createdOn;
