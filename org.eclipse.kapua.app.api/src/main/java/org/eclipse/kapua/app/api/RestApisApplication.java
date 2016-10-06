822d0a4e6e78f86e52f8e7239a3ddb07f97f6645
stefanomorson <stefano.morson@eurotech.com>
2016-10-06 11:07:29 +0200

    Chng - #227 Api - Make the moxy JAXB context available to inner classes

--- org.eclipse.kapua.app.api/src/main/java/org/eclipse/kapua/app/api/RestApisApplication.java
+++ org.eclipse.kapua.app.api/src/main/java/org/eclipse/kapua/app/api/RestApisApplication.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2011, 2016 Eurotech and/or its affiliates and others
+ * Copyright (c) 2011, 2016 Eurotech and/or its affiliates
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
@@ -16,9 +16,14 @@ import java.util.HashMap;
 import javax.ws.rs.core.MediaType;
 import javax.xml.bind.JAXBException;
 
+import org.eclipse.kapua.KapuaException;
+import org.eclipse.kapua.commons.util.xml.XmlUtil;
+import org.glassfish.hk2.api.ServiceLocator;
 import org.glassfish.jersey.server.ResourceConfig;
 import org.glassfish.jersey.server.ServerProperties;
 import org.glassfish.jersey.server.filter.UriConnegFilter;
+import org.glassfish.jersey.server.spi.Container;
+import org.glassfish.jersey.server.spi.ContainerLifecycleListener;
 
 import io.swagger.jaxrs.listing.ApiListingResource;
 import io.swagger.jaxrs.listing.SwaggerSerializers;
@@ -33,15 +38,37 @@ public class RestApisApplication extends ResourceConfig {
 		HashMap<String, MediaType> mappedMediaTypes = new HashMap<String, MediaType>();
 		mappedMediaTypes.put("xml", MediaType.APPLICATION_XML_TYPE);
 		mappedMediaTypes.put("json", MediaType.APPLICATION_JSON_TYPE);
-		
+
 		property(ServerProperties.MEDIA_TYPE_MAPPINGS, mappedMediaTypes);
 		register(UriConnegFilter.class);
 		register(JaxbContextResolver.class);
-		register(KapuaEntityBodyWriter.class);
-		register(KapuaListResultBodyWriter.class);
+		register(RestApiJAXBContextProvider.class);
+		register(KapuaSerializableBodyWriter.class);
 
 		// Hook the swagger-ui
-		registerClasses(ApiListingResource.class, 
+		registerClasses(ApiListingResource.class,
 						SwaggerSerializers.class);
+
+		register(new ContainerLifecycleListener() {
+
+			@Override
+			public void onStartup(Container container) {
+				ServiceLocator serviceLocator = container.getApplicationHandler().getServiceLocator();
+
+				RestApiJAXBContextProvider provider = serviceLocator.createAndInitialize(RestApiJAXBContextProvider.class);
+ 				XmlUtil.setContextProvider(provider);
+			}
+
+			@Override
+			public void onReload(Container container) {
+				//Nothing todo
+			}
+
+			@Override
+			public void onShutdown(Container container) {
+				//Nothing todo
+			}
+
+		});
 	}
 }
---
+++ org.eclipse.kapua.service/src/main/java/org/eclipse/kapua/KapuaSerializable.java
@@ -0,0 +1,8 @@
+package org.eclipse.kapua;
+
+/**
+ * Marker interface for identifying all Kapua Serializable.
+ */
+public interface KapuaSerializable {
+
+}
