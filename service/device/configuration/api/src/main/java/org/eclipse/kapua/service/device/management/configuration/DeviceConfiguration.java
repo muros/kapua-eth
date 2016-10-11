2f308e4f7a73e075859cbe4b0ae6eac3fac7f14c
Claudio Mezzasalma <claudio.mezzasalma@eurotech.com>
2016-10-11 14:58:50 +0200

    Fixed missing semicolon

--- service/device/configuration/api/src/main/java/org/eclipse/kapua/service/device/management/configuration/DeviceConfiguration.java
+++ service/device/configuration/api/src/main/java/org/eclipse/kapua/service/device/management/configuration/DeviceConfiguration.java
@@ -18,7 +18,7 @@ import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlRootElement;
 import javax.xml.bind.annotation.XmlType;
-import javax.xml.bind.annotation.XmlElement
+import javax.xml.bind.annotation.XmlElement;
 
 import org.eclipse.kapua.KapuaSerializable;
 
