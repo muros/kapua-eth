4083a0f0f55da95a3bf0fa2a4b5c81e6809b71d9
Uros Mesaric <githuburos@gmail.com>
2016-10-12 08:38:43 +0200

    Pom merge and start.sh dos2unix line endings.

--- org.eclipse.kapua.dev-tools/src/main/vagrant/start.sh
+++ org.eclipse.kapua.dev-tools/src/main/vagrant/start.sh
@@ -11,7 +11,7 @@
 #     Eurotech - initial API and implementation
 #
 #******************************************************************************
-if [ "$#" -lt 1 ]; 
+if [ "$#" -lt 1 ];
 then
 	echo '======> missing input parameter. please specify develop/demo/base-box'
  	exit 2
@@ -20,7 +20,7 @@ if [ $1 = 'develop' ]
 then
 	echo '======> start develop vagrant machine'
 	sh develop/start-develop.sh
-elif [ $1 = 'demo' ] 
+elif [ $1 = 'demo' ]
 then
 	echo '======> start demo vagrant machine'
 	sh demo/start-demo.sh
